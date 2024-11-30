21113300d43f86878a0a744f95e7c3e9c0a6aa19
Uexcel2022 <excellenceudoka@gmail.com>
2024-11-30 21:43:22 +0100

    added change password endpoints

--- src/main/java/com/uexcel/library/exception/UnauthorizedException.java
+++ src/main/java/com/uexcel/library/exception/UnauthorizedException.java
@@ -3,8 +3,11 @@ package com.uexcel.library.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.io.Serial;
+
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class UnauthorizedException extends RuntimeException {
+    @Serial
     private static final long serialVersionUID = 1L;
     public UnauthorizedException(String message) {
         super(message);
